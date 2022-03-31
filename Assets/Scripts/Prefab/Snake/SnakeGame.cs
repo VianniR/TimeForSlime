7e1c4d671d215c91641833b821b72d02481b618a
ThisPerson101 <101739803+ThisPerson101@users.noreply.github.com>
2022-03-31 16:15:54 -0700

    snake vectors time

--- Assets/Scripts/Prefab/Snake/SnakeGame.cs
+++ Assets/Scripts/Prefab/Snake/SnakeGame.cs
@@ -5,23 +5,48 @@ using UnityEngine;
 public class SnakeGame : MonoBehaviour
 {
     private GameObject player;
-    public GameObject snake;
+    Vector2 direction;
+    public float speed;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        direction = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        player.transform.position = transform.position;
+        if (xInput == 1)
+        {
+            direction = Vector2.right;
+        }
+        else if (xInput == -1)
+        {
+            direction = Vector2.left;
+        }
+        else if (yInput == 1)
+        {
+            direction = Vector2.up;
+        }
+        else if (yInput == -1)
+        {
+            direction = Vector2.down;
+        }
+
+        rb.velocity = speed * direction;
     }
 
     public void SpawnSnake(Transform entrance)
     {
-        snake.SetActive(true);
-        snake.transform.position = entrance.position;
+        player.SetActive(false);
+        gameObject.SetActive(true);
+        transform.position = entrance.position;
     }
 
 }
