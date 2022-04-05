7fe090fc967d2110b549c6240bb7c9a1d47576e8
ThisPerson101 <101739803+ThisPerson101@users.noreply.github.com>
2022-04-05 11:00:18 -0700

    snakwd z

--- Assets/Scripts/Prefab/Snake/SnakeGame.cs
+++ Assets/Scripts/Prefab/Snake/SnakeGame.cs
@@ -17,6 +17,8 @@ public class SnakeGame : MonoBehaviour
     Vector3Int prevTilemapPos;
 
     Vector3 lastEntrance = new Vector2(0, 0);
+
+    public GameObject currSnakeTile;
     // Start is called before the first frame update
     void Awake()
     {
@@ -33,36 +35,53 @@ public class SnakeGame : MonoBehaviour
         player.transform.position = transform.position;
         if (xInput == 1 && !direction.Equals(Vector2.right))
         {
+            CreateBend(direction, Vector2.right);
             direction = Vector2.right;
             transform.position = CellToRealWorld(tilemapPos);
         }
         else if (xInput == -1 && !direction.Equals(Vector2.left))
         {
+            CreateBend(direction, Vector2.left);
             direction = Vector2.left;
             transform.position = CellToRealWorld(tilemapPos);
         }
         else if (yInput == 1 && !direction.Equals(Vector2.up))
         {
+            CreateBend(direction, Vector2.up);
             direction = Vector2.up;
             transform.position = CellToRealWorld(tilemapPos);
         }
         else if (yInput == -1 && !direction.Equals(Vector2.down))
         {
+            CreateBend(direction, Vector2.down);
             direction = Vector2.down;
             transform.position = CellToRealWorld(tilemapPos);
         }
+        if(timer < 0)
+            rb.velocity = speed * direction;
 
-        rb.velocity = speed * direction;
-        if(!prevTilemapPos.Equals(tilemapPos))
+        if (!prevTilemapPos.Equals(tilemapPos))
         {
             float cosTheta = (direction.x + 1);
             float sinTheta = (direction.y + direction.x * (direction.x - 1));
-            Instantiate(snake, CellToRealWorld(tilemapPos), new Quaternion(0, 0, sinTheta, cosTheta));
+            currSnakeTile = Instantiate(snake, CellToRealWorld(tilemapPos), new Quaternion(0, 0, sinTheta, cosTheta));
         }
 
         if (xInput != 0 || yInput != 0)
             timer = -2;
         prevTilemapPos = tilemapPos;
+    }
+
+    public void CreateBend(Vector2 prevDir, Vector2 newDir)
+    {
+        float side = (Vector3.Cross(prevDir, newDir)).z;
+        float cosTheta = (prevDir.x + 1);
+        float sinTheta = (prevDir.y + prevDir.x * (prevDir.x - 1));
+        GameObject newBend = Instantiate(snake, currSnakeTile.transform.position, new Quaternion(0, 0, sinTheta, cosTheta));
+        newBend.transform.localScale = new Vector3(-1, side, 1) * 0.5f;
+        newBend.GetComponent<Animator>().Play("Bend");
+        Destroy(currSnakeTile);
+        currSnakeTile = newBend;
     }
 
     public void SpawnSnake(Transform entrance)
@@ -98,9 +117,8 @@ public class SnakeGame : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            direction = Vector2.zero;
             if (timer < 0)
-                timer = 0.5f;
+                timer = 1f;
         }
     }
 
